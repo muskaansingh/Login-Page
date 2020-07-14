45f9217cd9989265c549ad837802bcfa4e3ac131
Muskaan Singh <muskaansingh235@gmail.com>
2020-07-14 23:35:32 +0530

    Updates

---
+++ precache-manifest.088e85b93b501bd50238ac0db3bd45f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a34156261b6494be1330cbe3e367976a",
+    "url": "/Login-Page/index.html"
+  },
+  {
+    "revision": "649e50d83591af50a509",
+    "url": "/Login-Page/static/css/main.011bf109.chunk.css"
+  },
+  {
+    "revision": "7df92799459c606c321f",
+    "url": "/Login-Page/static/js/2.7dfef08f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Login-Page/static/js/2.7dfef08f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "649e50d83591af50a509",
+    "url": "/Login-Page/static/js/main.6579dd18.chunk.js"
+  },
+  {
+    "revision": "81e4631a5375cf39852f",
+    "url": "/Login-Page/static/js/runtime-main.0fc7ed13.js"
+  },
+  {
+    "revision": "9a76e74e107e6163dc82116b8236c132",
+    "url": "/Login-Page/static/media/login.9a76e74e.svg"
+  }
+]);
